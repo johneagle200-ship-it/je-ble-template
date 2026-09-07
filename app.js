f740548b5675d01f126e73b20c418a6cdf52418e
johneagle200-ship-it <johneagle200@gmail.com>
2026-09-07 11:15:11 +0300

    Refactor MainApp to extend BaseApp
    Refactor MainApp class to extend BaseApp and remove init and loadAppVersion methods.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-class MainApp extends BaseBLEDevice {
+class MainApp extends BaseApp {
   constructor() {
     super({
       repoOwner: "johneagle200-ship-it",
@@ -6,34 +6,7 @@ class MainApp extends BaseBLEDevice {
     });
   }
 
-  async init() {
-    // 1. Отображаем версию веб-приложения сразу при запуске
-    this.loadAppVersion();
-
-    // 2. Запускаем инициализацию BLE базового класса
-    if (super.init) {
-      await super.init();
-    }
-  }
-
-  async loadAppVersion() {
-    try {
-      const res = await fetch('./package.json');
-      const pkg = await res.json();
-      const ver = `v${pkg.version}`;
-
-      const headerEl = document.getElementById('appVersionHeader');
-      const menuEl = document.getElementById('appVersion');
-
-      if (headerEl) headerEl.innerText = ver;
-      if (menuEl) menuEl.innerText = ver;
-    } catch (err) {
-      console.error('Ошибка загрузки package.json:', err);
-    }
-  }
-
   onTelemetry(data) {
-    // 1. Выводим счётчик пакетов от ESP32 в центральный блок
     if (data.counter !== undefined) {
       const el = document.getElementById('telemetryData');
       if (el) el.innerText = `# ${data.counter}`;
@@ -42,7 +15,6 @@ class MainApp extends BaseBLEDevice {
       if (el) el.innerText = data.val;
     }
 
-    // 2. Выводим время работы ESP32 (в секундах)
     if (data.uptime !== undefined) {
       const uptimeEl = document.getElementById('uptimeData');
       if (uptimeEl) uptimeEl.innerText = `${data.uptime} с`;
